22b8dde1133920e7592ac32137a6432861bf5004
Jean Nguyen <community@thebeanfamily.org>
2023-11-18 06:40:33 +0700

    Update vietqr.js

--- vietqr.js
+++ vietqr.js
@@ -8,5 +8,5 @@ function include(file) {
     document.getElementsByTagName('head').item(0).appendChild(script);
  
 }
-include('https://cdn.jsdelivr.net/gh/beanfamily/vietqr-payment-js@main/index.js')
-include('https://cdn.jsdelivr.net/gh/beanfamily/vietqr-payment-js@main/qrcode.js')
+include('https://cdn.jsdelivr.net/gh/unicubevn/vietqr-payment-js@main/index.js')
+include('https://cdn.jsdelivr.net/gh/unicubevn/vietqr-payment-js@main/qrcode.js')
