4dab3ca5e6f9e0f943ef3cff6ad167e1040f7fc4
Jean Nguyen <community@thebeanfamily.org>
2023-11-18 07:21:50 +0700

    Update vietqr.js

--- vietqr.js
+++ vietqr.js
@@ -2,7 +2,7 @@ function include(file) {
  
     let script = document.createElement('script');
     script.src = file;
-    script.type = 'text/javascript';
+    script.type = 'module';
     script.defer = true;
  
     document.getElementsByTagName('head').item(0).appendChild(script);
